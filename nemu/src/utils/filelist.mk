a1cd9f70f718ff2d6d17097ace4b9af3e7998601
ysyx22051086-Xia Tengfei <xiatengfei20@mails.ucas.ac.cn>
2023-07-16 21:38:25 +0800

    use llvm-11

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -15,6 +15,6 @@
 
 ifdef CONFIG_ITRACE
 CXXSRC = src/utils/disasm.cc
-CXXFLAGS += $(shell llvm-config-13 --cxxflags) -fPIE
-LIBS += $(shell llvm-config-13 --libs)
+CXXFLAGS += $(shell llvm-config-11 --cxxflags) -fPIE
+LIBS += $(shell llvm-config-11 --libs)
 endif
